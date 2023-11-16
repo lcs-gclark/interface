c2c094fc7628b8d2cf27614bc5bf72cfd65cd318
grayden clark <gclark@lcs.on.ca>
2023-11-16 08:54:56 -0500

    Initial Commit

---
+++ interface/interfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  interfaceApp.swift
+//  interface
+//
+//  Created by Gray Clark on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct interfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
